249a5545df9321b266ec17d6f31a0f3cfde9b2e8
Sandy K <skuan8@my.bcit.ca>
2020-12-17 10:15:46 -0800

    update index

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-require('./db/mongoose');
-const PORT = process.env.PORT || 8000;
 const authRouter = require('./routes/authRoute');
 
+require('./db/mongoose');
+const PORT = process.env.PORT || 8000;
+
 app.use(bodyParser.json());
-app.use('/', authRouter);
+app.use('/user', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
