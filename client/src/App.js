e8f0cca178dbb53bd0a48c19798711e72c653e7c
Sandy K <skuan8@my.bcit.ca>
2020-12-17 16:52:34 -0800

    add css

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import UserForm from './components/UserForm/UserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <UserForm />
+      </div>
+    );
+  }
 }
 
 export default App;
