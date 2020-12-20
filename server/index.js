819df6a982edcf86a4a7135f3106da76f752ca20
Sandy K <skuan8@my.bcit.ca>
2020-12-19 21:55:28 -0800

    add bodyparser

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const authRouter = require('./routes/authRoute');
-
 require('./db/mongoose');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/userRoute');
+
+const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(bodyParser.json());
-app.use('/user', authRouter);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
