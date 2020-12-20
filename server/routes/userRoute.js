7487108264d490b7b5093ff4daf7a05c0da8b0a8
Sandy K <skuan8@my.bcit.ca>
2020-12-20 14:23:17 -0800

    generate authentication tokens for signing up and logging in

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -7,20 +7,21 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
 
   try {
+    const token = await user.generateAuthToken();
     await user.save();
-    res.status(201).send(user);
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-// Login
+// Login -- TESTED
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password);
-    res.send(user);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
-    console.log(e);
     res.status(400).send();
   }
 });
