02f4e307f4519c51d687066b922573c202490972
Sandy K <skuan8@my.bcit.ca>
2020-12-20 17:39:59 -0800

    generate auth tokens

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Schema = mongoose.Schema;
 
@@ -27,13 +28,32 @@ const UserSchema = new Schema({
     required: true,
     minLength: 4,
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
   created_date: {
     type: Date,
     default: Date.now,
   },
 });
 
-// Find users
+// Generate token
+UserSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisismysecret');
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+// Find user and compare passwords
 UserSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
   if (!user) {
