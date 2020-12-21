9596197f2522142b8ff58fba69fede96611189cb
Sandy K <skuan8@my.bcit.ca>
2020-12-20 17:42:00 -0800

    register middleware

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ const userRouter = require('./routes/userRoute');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Register Mdiddleware
+app.use((req, res, next) => {
+  console.log(req.method, req.path);
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
