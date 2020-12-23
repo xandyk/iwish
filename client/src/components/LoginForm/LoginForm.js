5da8f5293dfb30956f196afc1d773f31a9f42eb2
Sandy K <skuan8@my.bcit.ca>
2020-12-23 09:13:47 -0800

    modify login form

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -3,28 +3,44 @@ import React from 'react';
 const Login = () => {
   return (
     <>
-      <form className="ui form">
-        <div className="segment login">
-          <h2>Sign in to your account</h2>
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="email">
-            Email
-          </label>
-          <input className="label" type="text" />
-        </div>
+      <section className="container">
+        <div className="segment">
+          <h2>Login to your account</h2>
+          <form className="ui form" onSubmit={this.onLoginSubmit}>
+            <div className="field">
+              <label className="label email" htmlFor="email">
+                Email
+              </label>
+              <input
+                type="text"
+                value={this.state.email}
+                onChange={e => {
+                  this.setState({ email: e.target.value });
+                }}
+                required
+              />
+            </div>
 
-        <div className="field">
-          <label className="label" htmlFor="password">
-            Password
-          </label>
-          <input type="text" />
-        </div>
+            <div className="field">
+              <label className="label password" htmlFor="password">
+                Password
+              </label>
+              <input
+                type="text"
+                value={this.state.password}
+                onChange={e => {
+                  this.setState({ password: e.target.value });
+                }}
+                required
+              />
+            </div>
 
-        <button className="button signin" type="submit">
-          Sign in
-        </button>
-      </form>
+            <button className="button register" type="submit">
+              Sign In
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 };
